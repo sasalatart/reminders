56a26722a700503df64afda51a76b901deb7a2c9
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-01-10 16:13:54 -0300

    parse server params from CamelCase to SnakeCase and vice-versa

--- server/config.ru
+++ server/config.ru
@@ -1,7 +1,13 @@
 require 'rack'
 require 'rack/contrib'
+require 'rack/parser'
+require 'plissken'
 require_relative './lib/app'
 
 use Rack::PostBodyContentTypeParser
+
+use Rack::Parser, parsers: {
+  'application/json' => proc { |data| JSON.parse(data).to_snake_keys }
+}
 
 run Sinatra::Application
--- server/lib/handlers/handlers.rb
+++ server/lib/handlers/handlers.rb
@@ -4,3 +4,8 @@ before /^(?!\/(signup|login))/ do
   @current_user = User.find_by_jwt(request.env['HTTP_TOKEN'])
   halt 403, 'Not authenticated' unless @current_user
 end
+
+after do
+  pass unless content_type == 'application/json' && !response.body.empty?
+  body JSON.parse(response.body[0]).to_camelback_keys.to_json
+end

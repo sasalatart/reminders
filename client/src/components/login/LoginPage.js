78fb7da7bdb7e685fc67a8719539d522fdeecd65
Sebastián Salata Ruiz-Tagle <sa.salatart@gmail.com>
2017-01-13 19:55:12 -0300

    reduce code duplication between LoginPage and SignupPage

--- client/src/components/login/LoginPage.js
+++ client/src/components/login/LoginPage.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import LoginForm from './LoginForm.js';
 
-class LoginPage extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.onLogin = this.onLogin.bind(this);
-  }
-
-  onLogin(jwt) {
-    this.props.onLogin(jwt);
-  }
-
-  render() {
-    return(
-      <div className="login-page">
-        <h1 className="title is-1 has-text-centered">Login</h1>
-        <div className="column is-4 is-offset-4">
-          <LoginForm onLogin={this.onLogin} />
-        </div>
+const LoginPage = ({onLogin}) => {
+  return(
+    <div className="login-page">
+      <h1 className="title is-1 has-text-centered">Login</h1>
+      <div className="column is-4 is-offset-4">
+        <LoginForm onLogin={(jwt) => onLogin(jwt)} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default LoginPage;
--- client/src/components/signup/SignupPage.js
+++ client/src/components/signup/SignupPage.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import SignupForm from './SignupForm';
 
-class SignupPage extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.onSignup = this.onSignup.bind(this);
-  }
-
-  onSignup(jwt) {
-    this.props.onSignup(jwt);
-  }
-
-  render() {
-    return(
-      <div className="signup-page">
-        <h1 className="title is-1 has-text-centered">Signup</h1>
-        <div className="column is-4 is-offset-4">
-          <SignupForm onSignup={this.onSignup} />
-        </div>
+const SignupPage = ({ onSignup }) => {
+  return(
+    <div className="signup-page">
+      <h1 className="title is-1 has-text-centered">Signup</h1>
+      <h2 className="subtitle has-text-centered">Keep track of your reminders.</h2>
+      <div className="column is-4 is-offset-4">
+        <SignupForm onSignup={jwt => onSignup(jwt)} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default SignupPage;
